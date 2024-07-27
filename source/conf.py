8bd344a35e1af9881b84557c5413d031f49ea058
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2024-07-27 16:22:27 +0200

    Upgrades

--- source/conf.py
+++ source/conf.py
@@ -76,10 +76,4 @@ html_show_sphinx = False
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 html_show_copyright = False
 
-
-# Intersphinx
-intersphinx_mapping = {
-    "red-hurdles": ("https://fire.tymyrddin.dev/", None),
-    "red-iac": ("https://iac.tymyrddin.dev/", None),
-}
 myst_url_schemes = ["http", "https", ]
